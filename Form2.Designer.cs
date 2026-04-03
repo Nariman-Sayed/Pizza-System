8ca1fc01be77c1c06ae50d9e9df4431c9da00023
Nariman-Sayed <162900760+Nariman-Sayed@users.noreply.github.com>
2026-04-03 15:45:57 +0200

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Pizza
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnOrderNow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Red;
+            this.lblTitle.Location = new System.Drawing.Point(503, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(279, 36);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Online Pizza Store";
+            // 
+            // btnOrderNow
+            // 
+            this.btnOrderNow.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnOrderNow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOrderNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrderNow.Location = new System.Drawing.Point(12, 108);
+            this.btnOrderNow.Name = "btnOrderNow";
+            this.btnOrderNow.Size = new System.Drawing.Size(137, 84);
+            this.btnOrderNow.TabIndex = 1;
+            this.btnOrderNow.Text = "Order Now !";
+            this.btnOrderNow.UseVisualStyleBackColor = false;
+            this.btnOrderNow.Click += new System.EventHandler(this.btnOrderNow_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BackgroundImage = global::Pizza.Properties.Resources.pizza_2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1284, 579);
+            this.Controls.Add(this.btnOrderNow);
+            this.Controls.Add(this.lblTitle);
+            this.DoubleBuffered = true;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnOrderNow;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizza
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrderNow_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+
+             
+            f1.Show();
+        }
+    }
+}
